b5eeb264ea223ffa8665432a79e9b7fa1884e900
Otavio <otaviomiranda@usp.br>
2024-12-29 19:43:49 -0300

    implementa links gerados

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,10 @@
 import axios from 'axios';
 import express, { Request, Response } from 'express';
 import dotenv from 'dotenv';
+import puppeteer from 'puppeteer';
 dotenv.config();
 const app = express();
-const port = 3000;
+const port = 3015;
 app.use(express.json());
 
 async function getSpotifyToken() {
@@ -30,23 +31,41 @@ async function getSpotifyToken() {
   }
 }
 
-app.get('/playlist', async function (req: Request, res: Response) {
+app.post('/playlist', async function (req: Request, res: Response) {
   try {
     const token = await getSpotifyToken();
-    const playlist = '62w0vYw7cTiWlb6d5eAjPv';
+    const playlist = req.body.playlist;
     const fields = "fields=items%28track%28name%2C+album%28name%29%2C+artists%28name%29%29%29"
     const url = `https://api.spotify.com/v1/playlists/${playlist}/tracks?${fields}`;
     const response: any = await axios.get(url, {
       headers: { Authorization: `Bearer ${token}` },
     }); 
-    const infos = [];
+    const musicInfos: musicInfos[]  = [];    
     for (const item of response.data.items) {
       const albumName = item.track.album.name;
       const artistName = item.track.artists.map((artist: any) => artist.name);
       const musicName = item.track.name;
-      infos.push({ musicName, artistName, albumName })
+      musicInfos.push({ musicName, artistName, albumName })
+    };    
+    const youtubeLinks: string[] = [];
+    for (const info of musicInfos) {
+      const browser = await puppeteer.launch();
+      const page = await browser.newPage();
+      const urlYoutube = createYoutubeUrl(info);
+      await page.goto(urlYoutube);
+      const hrefs = await page.evaluate(() => Array.from(
+            document.querySelectorAll('a[href]'),
+            a => a.getAttribute('href')
+          )
+        );
+      const links = hrefs.filter(href => href?.includes('watch'));
+      browser.close();      
+      if (!links[0]) { 
+        throw new Error("Não foi encontrado link para a musica");
+      };
+      youtubeLinks.push(links[0]);
     }
-    res.json(infos);
+    res.json(youtubeLinks);
   } catch (error: any) {
     console.error('Erro ao buscar playlist:', error.message);
 
@@ -63,3 +82,18 @@ app.get('/playlist', async function (req: Request, res: Response) {
 app.listen(port, () => {
   console.log(`Express is listening at http://localhost:${port}`);
 });
+
+function createYoutubeUrl(musicInfos: musicInfos): string {
+  const baseUrl = "https://www.youtube.com/results?search_query=";
+  const artist = musicInfos.artistName.toString().replaceAll(" ", "+").replaceAll(",", "+");
+  const album = musicInfos.albumName.replaceAll(" ", "+");
+  const music = musicInfos.musicName.replaceAll(" ", "+");
+  const urlYoutube = `${baseUrl}+${music}+${artist}+${album}`;
+  return urlYoutube;
+};
+
+type musicInfos = {
+  artistName: string[],
+  musicName: string,
+  albumName: string
+};
--- test/api.test.ts
+++ test/api.test.ts
@@ -1,40 +1,11 @@
 import axios from "axios"
 import puppeteer from "puppeteer";
 
-test("Deve fazer uma requisição e mostrar a playlist do usuario", async () => {
-  const response = await axios.get("http://localhost:3000/playlist");
-  console.debug(response.data);
-  expect(response.data.length).toBeGreaterThan(0);
-});
-
-test("Deve procurar com a lib puppeteer o link do video no youtube e achar o correto", async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  const url = "https://www.youtube.com/results?search_query=paralelas+belchior+Coração+Selvagem"
-  await page.goto(url);
-  const hrefs = await page.evaluate(() => Array.from(
-        document.querySelectorAll('a[href]'),
-        a => a.getAttribute('href')
-      )
-    );
-  const links = hrefs.filter(href => href?.includes('watch'))
-  browser.close()
-  expect(links[2]).toBe("/watch?v=fVNUe4np2SM&pp=ygUlcGFyYWxlbGFzIGJlbGNoaW9yIENvcmHDp8OjbyBTZWx2YWdlbQ%3D%3D");
-  
-  const browser2 = await puppeteer.launch();
-  const page2 = await browser2.newPage();
-  const url2 = "https://www.youtube.com/results?search_query=Onde+Anda+Voce++Toquinho++Vinícius+de+Moraes+20+Grandes+Sucessos+De+Toquinho+%26+Vinicius";
-  await page2.goto(url2);
-  const hrefs2 = await page2.evaluate(() => Array.from(
-    document.querySelectorAll('a[href]'),
-    a => a.getAttribute('href')
-      )
-    );
-  const links2 = hrefs2.filter(href => href?.includes('watch'))
-  browser2.close()
-  expect(links2[2]).toContain("/watch?v=Gb5sbORA62w");
-}, 20000);
-
-test.only('Deve receber os valores da musica e gerar o link correto', async () => {
-  
-})
+test.only("Deve enviar uma playlist do spotfy e receber os links de cada respectiva musica no youtube", async () => {
+  const input = { playlist: '62w0vYw7cTiWlb6d5eAjPv' };
+  const response = await axios.post("http://localhost:3015/playlist", input);
+  expect(response.data.length).toBe(37);
+  expect(response.data[0]).toContain('/watch?v=XTb9GNIxpMk');
+  expect(response.data[1]).toContain('/watch?v=w1WO-jkbcrY');
+  expect(response.data[2]).toContain('/watch?v=UsXioRxVGO8');
+}, 1200000);
